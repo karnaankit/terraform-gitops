d219df49cbf82c5450134d06f601370f6237c1cd
Ankit Karna <40463873+karnaankit@users.noreply.github.com>
2024-04-04 06:29:39 +0545

    terraform files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "tf_backend_bucket_name" {
+  default = "ankit-gitops-terraform-state"
+}
+
+variable "bucket" {
+  default = "ankit-gitops-terraform"
+}
